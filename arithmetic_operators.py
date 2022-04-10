5ed88a0abcf703f6f0ae6bce3b3d7e0b765439b8
tanvirahmed007 <tanvir15-9909@diu.edu.bd>
2022-04-10 10:54:28 +0600

    Added

---
+++ arithmetic_operators.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    print("{}".format(a+b))
+    print("{}".format(a-b))
+    print("{}".format(a*b))
---
+++ division.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    print("{}".format(a//b))
+    print("%f"%float(a/b))
