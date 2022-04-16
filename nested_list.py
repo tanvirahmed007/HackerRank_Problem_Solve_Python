4a96fc41197612894e1bde392c778559ede20a1e
osmanforhad <osmanforhad93@gmail.com>
2022-04-16 11:15:28 +0600

    Second Minimum

---
+++ nested_list.py
@@ -0,0 +1,9 @@
+score_list = []; 
+for _ in range(int(input())):
+    name = input()
+    score = float(input())
+    
+    
+    score_list.append([name, score])
+second_highest = sorted(set([score for name, score in score_list]))[1]
+print('\n'.join(sorted([name for name, score in score_list if score == second_highest])))
